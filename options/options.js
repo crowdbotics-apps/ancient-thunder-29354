6731ed24c2b9d9a20ffd6e33f301f73d99e208fe
Crowdbotics <team@crowdbotics.com>
2021-08-02 23:04:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ancient_thunder_29354Identifier",
+  url: "https://ancient_thunder_29354Identifier.botics.co",
+  api: "https://ancient_thunder_29354Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
